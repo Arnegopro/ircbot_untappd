fea7f2d7682c9f8da321d39b32f15ae3779c745c
arnegopro <arnem.sund@gmail.com>
2016-07-13 17:04:09 +0200

    Meta stuffs

--- checkfornewbeers.sh
+++ checkfornewbeers.sh
@@ -2,6 +2,7 @@
 sh get_profile.sh
 for i in $(cat namelist); do
     IFS=$'\n'
+    beerslast5min=0
     for j in $(cat $i.profile | grep -Eo "([0-9]{2} [A-Za-z]{3} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2})"); do
 
         # adjust timezone
@@ -17,6 +18,14 @@ for i in $(cat namelist); do
         beer=$(date --date=$j +%s)
         if [[ $(($now - $beer)) -le 300 ]]; then
             echo "new beerz for $i"
-        fi;
+	    let "beerslast5min += 1"
+	    echo "Beers = $beerslast5min"
+	    echo "time = $j"
+	    #Getting metadata from profile
+	    checkinID=`( cat $i.profile | grep $j |grep -Eo "([0-9]{9})" )`
+	    meta=`( cat $i.profile | grep -A 15 'id="checkin_'$checkinID )` 
+            meta2=`( echo $meta | grep -oP '(?<=class="user">).*(?=/p)' )`	    
+	    echo $meta2
+	fi;
     done;
 done;
